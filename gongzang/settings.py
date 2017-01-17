3afe2f00897cb7b5aa388d80e3abbeadc1a87576
pacebook <haegyun821@gmail.com>
2017-01-17 15:27:24 +0900

    psycopg2 set

--- gongzang/settings.py
+++ gongzang/settings.py
@@ -95,13 +95,15 @@ WSGI_APPLICATION = 'gongzang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mydb',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'postgre',
+        'PASSWORD': os.environ['GONGJANG_SET_1'],
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
-
-
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -130,3 +132,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 
 MEDIA_ROOT = 'upload_files'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
